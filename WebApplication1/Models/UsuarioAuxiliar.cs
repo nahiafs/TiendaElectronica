e2322e9f3e7e108788661abeb67ba1329b35ab0d
Felipe Alejandro Borjas de los Dolores <felipeborjasdam20@iescastelar.com>
2023-01-25 10:59:31 +0100

    Boton añadir

---
+++ WebApplication1/Models/UsuarioAuxiliar.cs
@@ -0,0 +1,22 @@
+﻿using MusicaLMFL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Libreria_V6.Models
+{
+    public class UsuarioAuxiliar
+    {
+        public List<TProducto> producto { get; set; }
+
+        public UsuarioAuxiliar(List<TProducto> producto)
+        {
+            this.producto = producto;
+        }
+
+        public UsuarioAuxiliar()
+        {
+        }
+    }
+}
