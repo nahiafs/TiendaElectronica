2502c857e5dc4391193c2a0a08e75c1eaffb7e54
PriceST1 <falejandro.borjas@gmail.com>
2023-02-14 19:15:33 +0100

    Documentado metodo init para que no se me olvide

--- WebApplication1/Javascript/Plantilla.js
+++ WebApplication1/Javascript/Plantilla.js
@@ -1,5 +1,8 @@
 ﻿window.addEventListener("load", init);
 
+/*
+ Funcion que se encarga de darle valor al badge del boton carrito, el cual indicara con un numero los elementos que tiene el carrito
+ */
 function init() {
     let $numElementosCarrito = document.querySelector("#numElementsCarrito");
 
@@ -12,6 +15,12 @@ function init() {
         carrito = [];
         $numElementosCarrito.textContent = 0;
     } else {
+        /*
+         * Lo que hace aqui es una funcion callback con un sumatorio, 
+         * el metodo reduce lo que hace es pasar por parametros("elemento" en este caso) los elementos del array, 
+         * y total es el parametro que devolvera, en este caso lo que hacemos es para cada Objeto.Cantidad que esta dentro del array carrito, le sumamos el valor a total, que empezara en 0
+         * con esto, conseguimos que recorra todo el array de carrito y haga el sumatorio de los elementos Cantidad.
+        */
         let suma = carrito.reduce((total, elemento) => total + elemento[0].Cantidad, 0);
         $numElementosCarrito.textContent = suma;
     }
